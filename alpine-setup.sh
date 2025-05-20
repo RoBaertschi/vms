d7309593574457085ca1c792d4819cef1d0d107f
Robin Bärtschi <rtmbaertschi007@gmail.com>
2025-05-20 20:40:57 +0200

    changes

--- alpine-setup.sh
+++ alpine-setup.sh
@@ -8,10 +8,16 @@ set -ex
 
 rcser() {
     for service in "$@"; do
-        rc-update add $service
+        rc-update add $service default
         rc-service $service start
     done
 }
+
+# mkdir /etc/runlevels/async
+# rc-update add -s default async
+# sed '/::wait:\/sbin\/openrc default/a ::once:\/sbin\/openrc async -q' /etc/inittab > inittab.new
+# mv /etc/inittab /etc/inittab.bak
+# mv inittab.new /etc/inittab
 
 setup-keymap ch ch
 setup-hostname localhost
@@ -19,10 +25,10 @@ setup-interfaces -a -r eth0
 rc-service networking start
 setup-timezone -z Europe/Zurich
 rc-service hostname restart
-rc-update add networking boot
+rc-update add networking default
 rc-update add seedrng boot
-rc-update add acpid
-rc-update add crond
+rc-update add acpid default
+rc-update add crond default
 openrc boot
 openrc default
 
@@ -76,17 +82,60 @@ add_plugin() {
 
     mkdir -p ~/.local/state/nvim
     git clone $repo ~/.local/state/nvim/nvim/$name
-    NV_RUNTIME_PATH="$NV_RUNTIME_PATH ~/.local/state/nvim/$name"
+    NV_RUNTIME_PATH="$NV_RUNTIME_PATH $name"
 }
 
 add_plugin nvim-lspconfig https://github.com/neovim/nvim-lspconfig
 add_plugin nvim-treesitter https://github.com/nvim-treesitter/nvim-treesitter
+add_plugin mini.nvim https://github.com/echasnovaski/mini.nvim
 
 mkdir -p $XDG_CONFIG_HOME/nvim
 
+cat >> $XDG_CONFIG_HOME <<EOF
+vim.cmd [[colorscheme retrobox]]
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = "a"
+
+vim.schedule(function()
+>       vim.opt.clipboard = "unnamedplus"
+end)
+
+vim.opt.breakindent = true
+
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.timeoutlen = 300
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+
+vim.opt.inccommand = "split"
+
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 10
+
+vim.opt.signcolumn = "yes"
+
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>")
+vim.keymap.set("i", "<a-space>", function()
+>       vim.lsp.completion.get()
+end)
+EOF
+
 for dir in $NV_RUNTIME_PATH; do
     cat >> $XDG_CONFIG_HOME/nvim/init.lua <<EOF
-    vim.opt.rtp:append("$dir")
+    vim.opt.rtp:append(vim.fn.stdpath "state" .. "/$dir")
 EOF
 done
 
@@ -94,15 +143,15 @@ apk add nodejs npm
 
 LSPS=""
 
-add_npm_lsp() {
-    npm_name=$1
+add_apk_lsp() {
+    apk_name=$1
     lsp_name=$2
 
-    npm i -g $npm_name
+    apk add $apk_name
     LSPS="$LSPS $lsp_name"
 }
 
-add_npm_lsp bash-language-server bashls
+add_apk_lsp lua-language-server lua_ls
 
 for lsp in $LSPS; do
     cat >> $XDG_CONFIG_HOME/nvim/init.lua <<EOF
@@ -134,11 +183,32 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end
 })
 
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "markdown", "vimdoc", "vim", "lua" },
     highlight = {
         enable = true,
     },
+    auto_install = true,
+    indent = {
+        enable = true,
+        disable = { "ruby", "zig" },
+    },
 }
+
+vim.wo.foldtext = "v:lua.vim.treesitter.foldtext()"
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo.foldlevel = 99
+vim.opt.foldlevelstart = -1
+vim.opt.foldnestmax = 99
+
+require "mini.ai".setup({ n_lines = 500 })
+require "mini.surround".setup()
+local statusline = require "mini.statusline"
+statusline.setup({ use_icons = false })
+statusline.section_location = function()
+    return "%2l:%-2v"
+end
 EOF
 
 
@@ -168,6 +238,28 @@ done
 
 rcser cgroups dbus
 
+echo "Setting up greetd"
+apk add greetd greetd-tuigreet
+
+mkdir /etc/greetd
+cat > /etc/greetd/config.toml <<EOF
+[default_session]
+command = "tuigreet --cmd zsh"
+user = "greetd"
+[terminal]
+vt = 1
+EOF
+
+sed -i -e 's/tty1/# tty1/' /etc/inittab
+
+echo >> /etc/conf.d/greetd
+echo "rc_need=elogind" >> /etc/conf.d/greetd
+
+rc-update add greetd default
+
+echo "Parallel openrc"
+sed -i -e 's/#rc_parallel="NO"/rc_parallel="YES"/' /etc/rc.conf
+
 export USE_EFI=1
 echo "Creating disk, user input required."
 setup-disk -m sys /dev/vda
@@ -177,9 +269,9 @@ mount /dev/vda3 /mnt/root
 
 mkdir -p /mnt/root/home/robin
 
-cp -r $HOME/* /mnt/root/home/robin
+cp -r ~/* /mnt/root/home/robin
 cp -r ~/.config /mnt/root/home/robin/.config
-mkdir -p /mnt/root/home/robin/.local/state
-cp -r ~/.local/state/* /mnt/root/home/robin/.local/state
+mkdir -p /mnt/root/home/robin/.local/state/
+cp -r ~/.local/state/nvim/* /mnt/root/home/robin/.local/state/nvim
 
 chown -R robin:robin /mnt/root/home/robin
