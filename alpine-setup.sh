e898d8e590d4ea318e40726ab3ac63cc5a4d2004
RoBaertschi <rtmbaertschi007@gmail.com>
2025-05-20 15:03:00 +0200

    lots of changes

--- alpine-setup.sh
+++ alpine-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# mkdir -p /mnt/shared
+# mount -t 9p -o trans=virtio,version=9p2000.L shared /mnt/shared
+# cp /mnt/shared/alpine-setup.sh .
+
 set -ex
 
 rcser() {
@@ -22,27 +26,127 @@ rc-update add crond
 openrc boot
 openrc default
 
-/etc/apk/repositories <<EOF
+cat > /etc/apk/repositories <<EOF
 /media/sr0/apks
 http://dl-cdn.alpinelinux.org/alpine/edge/main
 http://dl-cdn.alpinelinux.org/alpine/edge/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 EOF
 
-apk update
+
+apk -U upgrade
 
 # setup-apkrepos -f -c
 setup-user -a -f robin -g audio,input,video,netdev robin
+mkdir -p /home/robin
+chown robin:robin /home/robin
 echo "Please create a password for user \"robin\""
 passwd robin
 echo "Please create a password for user \"root\""
 passwd root
 
+echo "Setup zsh"
+apk add zsh shadow
+chsh robin -s /bin/zsh
+
+mkdir -p /etc/zsh/zshrc.d
+echo 'PROMPT="%F{red}%n%f@%F{red}%m%f %F{green}%~$ %f"' > /etc/zsh/zshrc.d/prompt.zsh
+
+cat > /etc/profile.d/xdg.sh <<EOF
+export XDG_CONFIG_HOME=\$HOME/.config
+export XDG_CACHE_HOME=\$HOME/.cache
+export XDG_DATA_HOME=\$HOME/.local/share
+export XDG_STATE_HOME=\$HOME/.local/state
+export XDG_DATA_DIRS=/usr/local/share:/usr/share
+export XDG_CONFIG_DIRS=/etc/xdg
+EOF
+
+
+apk add neovim git tree-sitter-lua build-base
+
+NV_RUNTIME_PATH=""
+
+source /etc/profile
+
+# 1 Name
+# 2 Repository
+add_plugin() {
+    name=$1
+    repo=$2
+
+    mkdir -p ~/.local/state/nvim
+    git clone $repo ~/.local/state/nvim/nvim/$name
+    NV_RUNTIME_PATH="$NV_RUNTIME_PATH ~/.local/state/nvim/$name"
+}
+
+add_plugin nvim-lspconfig https://github.com/neovim/nvim-lspconfig
+add_plugin nvim-treesitter https://github.com/nvim-treesitter/nvim-treesitter
+
+mkdir -p $XDG_CONFIG_HOME/nvim
+
+for dir in $NV_RUNTIME_PATH; do
+    cat >> $XDG_CONFIG_HOME/nvim/init.lua <<EOF
+    vim.opt.rtp:append("$dir")
+EOF
+done
+
+apk add nodejs npm
+
+LSPS=""
+
+add_npm_lsp() {
+    npm_name=$1
+    lsp_name=$2
+
+    npm i -g $npm_name
+    LSPS="$LSPS $lsp_name"
+}
+
+add_npm_lsp bash-language-server bashls
+
+for lsp in $LSPS; do
+    cat >> $XDG_CONFIG_HOME/nvim/init.lua <<EOF
+    vim.lsp.enable("$lsp")
+EOF
+done
+
+cat >> $XDG_CONFIG_HOME/nvim/init.lua <<EOF
+vim.cmd[[set completeopt+=menuone,noselect,popup]]
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(ev)
+        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+        if client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, ev.buf, {
+                autotrigger = true,
+            })
+        end
+        if not client:supports_method('textDocument/willSaveWaitUntil')
+            and client:supports_method('textDocument/formatting') then
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            group = vim.api.nvim_create_augroup('my.lsp', {clear=false}),
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+            end,
+          })
+        end
+    end
+})
+
+require'nvim-treesitter.configs'.setup {
+    highlight = {
+        enable = true,
+    },
+}
+EOF
+
+
 echo "Setup openssh"
 setup-sshd -c openssh
 
 echo "Setup ntp"
-setup-ntp chronyd
+setup-ntp chrony
 
 echo "Setting up spice"
 apk add spice-vdagent spice-webdavd
@@ -66,4 +170,16 @@ rcser cgroups dbus
 
 export USE_EFI=1
 echo "Creating disk, user input required."
-setup-disk -m sys
+setup-disk -m sys /dev/vda
+
+mkdir /mnt/root
+mount /dev/vda3 /mnt/root
+
+mkdir -p /mnt/root/home/robin
+
+cp -r $HOME/* /mnt/root/home/robin
+cp -r ~/.config /mnt/root/home/robin/.config
+mkdir -p /mnt/root/home/robin/.local/state
+cp -r ~/.local/state/* /mnt/root/home/robin/.local/state
+
+chown -R robin:robin /mnt/root/home/robin
--- alpine-vm-setup.sh
+++ alpine-vm-setup.sh
@@ -1,95 +1,24 @@
 #!/bin/sh
 set -ex
 
-# TODO: Download iso image
+KERNEL=$(uname -s)
+case "$KERNEL" in
+    Darwin)
+        export PATH="$HOME/quickemu:$PATH"
+        ;;
+    *)
+        ;;
+esac
 
-NAME=minimim-vm
-ISO="alpine-virt-3.21.3-x86_64.iso"
-OVMF="/nix/store/9rfp4fqcdjhgkbc7c3ml2m6h8i2l08r3-OVMF-202411-fd/FV"
+quickget alpine v3.21
 
-if [ ! -d "${NAME}" ]; then
-    mkdir -p ${NAME}
-fi
-
-if [ ! -f "${NAME}/${NAME}" ]; then
-    qemu-img create -f qcow2 ${NAME}/${NAME}.qcow2 20G
-fi
-
-if [ ! -f "${NAME}/OVMF_VARS.fd" ]; then
-    cp ${OVMF}/OVMF_VARS.fd ${NAME}/OVMF_VARS.fd
-    chmod 755 ${NAME}/OVMF_VARS.fd
-fi
-
-# 1 chardev id
-# 2 chardev name
-# 3 device
-# 4 device id
-spicevmc() {
-    echo -n -chardev spicevmc,id=${1},name=${2} -device ${3},chardev=${1},id=${4}
-}
-
-REDIRS=""
-for redir in 1 2 3; do
-    REDIRS="$REDIRS $(spicevmc "usbredirchardev${redir}" "usbredir" "usb-redir" "usbredirdev${redir}")"
-done
-
-qemu-system-x86_64 \
-    -name ${NAME},process=${NAME} \
-    -machine q35,smm=off,vmport=off,accel=kvm \
-    -global kvm-pit.lost_tick_policy=discard \
-    -cpu host,topoext \
-    -smp cores=4,threads=2,sockets=1 \
-    -m 8G \
-    -device virtio-balloon \
-    -rtc base=utc,clock=host \
-    -vga none \
-    -device virtio-vga-gl,xres=1280,yres=800 \
-    -display sdl,gl=on \
-    -device virtio-rng-pci,rng=rng0 \
-    -object rng-random,id=rng0,filename=/dev/urandom \
-    -device qemu-xhci,id=spicepass \
-    $REDIRS \
-    -device pci-ohci,id=smartpass \
-    -device usb-ccid \
-    -chardev spicevmc,id=ccid,name=smartcard -device ccid-card-passthru,chardev=ccid \
-    -device usb-ehci,id=input \
-    -device usb-kbd,bus=input.0 \
-    -k de-ch \
-    -device usb-tablet,bus=input.0 \
-    -audiodev alsa,id=audio0 \
-    -device intel-hda \
-    -device hda-micro,audiodev=audio0 \
-    -device virtio-net,netdev=nic \
-    -netdev user,hostname=${NAME},hostfwd=tcp::22220-:22,smb=$HOME,id=nic \
-    -global driver=cfi.pflash01,property=secure,value=on \
-    -drive if=pflash,format=raw,unit=0,file=${OVMF}/OVMF_CODE.fd,readonly=on \
-    -drive if=pflash,format=raw,unit=1,file=${NAME}/OVMF_VARS.fd \
-    -drive media=cdrom,index=0,file=${ISO} \
-    -device virtio-blk-pci,drive=SystemDisk \
-    -drive id=SystemDisk,if=none,format=qcow2,file=${NAME}/${NAME}.qcow2 \
-    -fsdev local,id=fsdev0,path=/home/robin,security_model=mapped-xattr \
-    -device virtio-9p-pci,fsdev=fsdev0,mount_tag=Public-robin \
-    -monitor unix:${NAME}/${NAME}-montior.socket,server,nowait \
-    -serial unix:${NAME}/${NAME}-serial.socket,server,nowait
-
-cat <<EOF
-
-Quickemu 4.9.7 using /nix/store/h5g7vqw74zx9wj3s9kpm0vr8ybjkl5v2-qemu-9.2.3/bin/qemu-system-x86_64 v9.2.3
- - Host:     NixOS 25.11 (Xantusia) running Linux 6.12.21 deez-nix
- - CPU:      AMD Ryzen 7 5800X 8-Core Processor
- - CPU VM:   host, 1 Socket(s), 4 Core(s), 2 Thread(s)
- - RAM VM:   8G RAM
- - BOOT:     EFI (Linux), OVMF (/nix/store/9rfp4fqcdjhgkbc7c3ml2m6h8i2l08r3-OVMF-202411-fd/FV/OVMF_CODE.fd), SecureBoot (off).
- - Disk:     test2-alpine/disk.qcow2 (16G)
-             Just created, booting from alpine-v3.21/alpine-virt-3.21.3-x86_64.iso
- - Boot ISO: alpine-v3.21/alpine-virt-3.21.3-x86_64.iso
- - Display:  SDL, virtio-vga-gl, GL (on), VirGL (on) @ (1280 x 800)
- - Sound:    intel-hda (hda-micro)
- - ssh:      On host:  ssh user@localhost -p 22220
- - WebDAV:   On guest: dav://localhost:9843/
- - 9P:       On guest: sudo mount -t 9p -o trans=virtio,version=9p2000.L,msize=104857600 Public-robin ~/robin
- - smbd:     On guest: smb://10.0.2.4/qemu
- - Network:  User (virtio-net)
- - Monitor:  On host:  socat -,echo=0,icanon=0 unix-connect:test2-alpine/test2-alpine-monitor.socket
- - Serial:   On host:  socat -,echo=0,icanon=0 unix-connect:test2-alpine/test2-alpine-serial.socket
+cat > minimim-vm.conf <<EOF
+#!$(which quickemu)
+guest_os="linux"
+disk_img="minimim-vm/disk.qcow2"
+iso="alpine-v3.21/alpine-virt-3.21.3-x86_64.iso"
 EOF
+
+mkdir -p minimim-vm
+
+quickemu --vm minimim-vm.conf --extra_args "-virtfs local,path=$(pwd),mount_tag=shared,security_model=mapped-xattr"
